8ed8f1318dc90a5945e658bc7c3af195f1569217
codenoid <14269809+codenoid@users.noreply.github.com>
2024-03-15 06:09:50 +0700

    update hero

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,41 +12,23 @@
 	<title>Kartika Mas Tours &amp; Travel | Umroh dan Haji Plus Murah</title>
 </svelte:head>
 
-<div class="tour-hero bg-primary-3p">
-	<a
-		href="https://vimeo.com/115041822"
-		class="video-popup link d-grid tour-hero__el tour-hero__el-1"
-	>
-		<img src="/assets/img/video-btn.png" alt="image" class="img-fluid" />
-	</a>
+<div class="primary-hero">
 	<div class="container">
-		<div class="row g-4">
-			<div class="col-lg-8 col-xl-6">
-				<span
-					class="d-inline-flex align-items-center bg-primary-50 gap-4 rounded-pill py-1 ps-1 pe-5"
-				>
-					<span
-						class="w-9 h-9 rounded-circle d-grid place-content-center bg-primary-300 clr-neutral-0 flex-shrink-0"
-					>
-						<span class="material-symbols-outlined mat-icon fw-200"> arrow_right_alt </span>
-					</span>
-					<span class="h5 lh-1 m-0 clr-primary-400 fw-semibold"> tanah suci memanggil </span>
-				</span>
-				<h1 class="d2 mt-4 mb-6 max-text-16">
-					Jalankan Ibadah dengan <span class="clr-primary-300">Ketenangan</span>
-				</h1>
-				<p class="mb-5 fs-20">
-					Kartikamas mengakomodir kebutuhan umat dalam menjalankan ibadah mulai dari keberangkatan
-					hingga kepulangan dengan nyaman dan tenang.
+		<div class="row justify-content-center">
+			<div class="col-lg-8">
+				<h1 class="d2 text-center mb-6"><span class="clr-primary-300">Haji</span>, Umroh & <span class="clr-primary-300">Wisata</span> dengan KartikaMas</h1>
+				<p class="max-text-40 mx-auto text-center fs-20 mb-10">
+					Kartikamas mengakomodir kebutuhan umat mulai dari umroh, haji hingga penyewaan transportasi pariwisata.
 				</p>
 			</div>
-			<div class="col-xxl-12">
+			<div class="col-xxl-10">
 				<form action="/paket/umroh">
 					<div class="property-search p-6 rounded-3 bg-neutral-0">
 						<div
 							class="property-search__content d-flex flex-wrap justify-content-center align-items-center gap-4"
 						>
 							<div
+								style="display: none !important;"
 								class="property-search__select property-search__col rounded-pill d-flex align-items-center gap-2 px-6"
 							>
 								<span class="material-symbols-outlined mat-icon"> navigation </span>
@@ -72,7 +54,11 @@
 								class="property-search__select property-search__col rounded-pill d-flex align-items-center gap-2 px-6"
 							>
 								<span class="material-symbols-rounded mat-icon"> grid_view </span>
-								<select name="bandara_keberangkatan" class="form-select" aria-label="Default select example">
+								<select
+									name="bandara_keberangkatan"
+									class="form-select"
+									aria-label="Default select example"
+								>
 									<option selected>Bandara Keberangkatan</option>
 									<option value="cgk">CGK</option>
 								</select>
@@ -110,57 +96,51 @@
 						</div>
 					</div>
 				</form>
-				<!-- <div class="d-flex align-items-center gap-4 align-items-center flex-wrap mt-10">
-					<ul class="list user-group">
-						<li>
-							<div class="w-13 h-13 border border-neutral-0 rounded-circle overflow-hidden">
-								<img
-									src="https://i.pinimg.com/originals/2c/e8/14/2ce8149afc6347bcc437cc40bdafb67d.jpg"
-									alt="image"
-									class="w-100 h-100 object-fit-cover"
-								/>
-							</div>
-						</li>
-						<li>
-							<div class="w-13 h-13 border border-neutral-0 rounded-circle overflow-hidden">
-								<img
-									src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaWaW7q4R3s74oDOf7qhz9EBj2OTwS34Amg8IUoskkED3QSqCbnTfBC2vv6micHr3jq-A&usqp=CAU"
-									alt="image"
-									class="w-100 h-100 object-fit-cover"
-								/>
-							</div>
-						</li>
-						<li>
-							<div class="w-13 h-13 border border-neutral-0 rounded-circle overflow-hidden">
-								<img
-									src="https://storage.nu.or.id/storage/post/16_9/big/1604553085.jpg"
-									alt="image"
-									class="w-100 h-100 object-fit-cover"
-								/>
-							</div>
-						</li>
-						<li>
-							<div
-								class="w-13 h-13 border border-neutral-0 rounded-circle overflow-hidden position-relative"
-							>
-								<img
-									src="https://i.pinimg.com/originals/2c/e8/14/2ce8149afc6347bcc437cc40bdafb67d.jpg"
-									alt="image"
-									class="w-100 h-100 object-fit-cover"
-								/>
-								<span class="d-grid place-content-center position-absolute inset-0 bg-primary-70p">
-									<span class="material-symbols-outlined mat-icon clr-neutral-0"> add </span>
-								</span>
-							</div>
-						</li>
-					</ul>
-					<p class="mb-0 fs-18">
-						16 orang melakukan <span class="clr-primary-300 fw-semibold">booking</span> dalam 24jam terakhir
-					</p>
-				</div> -->
 			</div>
 		</div>
 	</div>
+	<img
+		src="https://pbs.twimg.com/media/DvrULZLWwAEipPc.jpg"
+		style="width: 508px;height: 642px;"
+		alt="makkah"
+		class="img-fluid primary-hero__el primary-hero__img-1"
+	/>
+	<img
+		src="https://storage2.kartikamas.id/vehicle/5181d686-dad4-4739-9ee5-d32cedcb206e.jpg"
+		style="width: 508px;height: 642px;"
+		alt="travel car"
+		class="img-fluid primary-hero__el primary-hero__img-2"
+	/>
+	<img
+		src="/assets/img/hotel-hero-el-4.png"
+		alt="2"
+		class="img-fluid primary-hero__el primary-hero__el-3"
+	/>
+	<img
+		src="/assets/img/hotel-hero-el-1.png"
+		alt="4"
+		class="img-fluid primary-hero__el primary-hero__el-4"
+	/>
+	<img
+		src="/assets/img/hotel-hero-el-3.png"
+		alt="1"
+		class="img-fluid primary-hero__el primary-hero__el-5"
+	/>
+	<img
+		src="/assets/img/hotel-hero-el-4.png"
+		alt="3"
+		class="img-fluid primary-hero__el primary-hero__el-6"
+	/>
+	<img
+		src="/assets/img/primary-hero-el-7.png"
+		alt="8"
+		class="img-fluid primary-hero__el primary-hero__el-7"
+	/>
+	<img
+		src="/assets/img/primary-hero-el-8.png"
+		alt="25"
+		class="img-fluid primary-hero__el primary-hero__el-8"
+	/>
 </div>
 
 <!-- Category Section  -->
